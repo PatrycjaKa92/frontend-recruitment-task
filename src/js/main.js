0f08c3fffa587492f526087266905a1c39691f7a
PatrycjaKa92 <58090630+PatrycjaKa92@users.noreply.github.com>
2022-10-16 16:36:28 +0200

    button reset

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,33 @@ const button = document.querySelector('.push');
 const popup = document.querySelector('.popup');
 const popupBackground = document.querySelector('.popup-background');
 const counterElement = document.querySelector('.counter');
-let counter = 0;
+const reset = document.querySelector('.reset');
+let counter = Number(localStorage.getItem('user') || 0);
 
 button.addEventListener('click', function() {
     popup.classList.add('show');
     counter = counter + 1;
-    counterElement.innerHTML = "You have clicked " + counter + " times to related button.";
+    localStorage.setItem('user', counter);
+    if (counter < 5) {
+        counterElement.innerHTML = "You have clicked <b>" + counter + " times </b>to related button.";
+    } else {
+        reset.classList.add('show');
+        counterElement.innerHTML = "You have clicked <b>" + counter + " times </b>to related button.";
+    }
 })
 
 // button.onclick =  function() {
 //     popup.classList.add('show');
 // }
 
+
+
 popupBackground.addEventListener('click', function() {
     popup.classList.remove('show');
+})
+
+reset.addEventListener('click', function() {
+    localStorage.removeItem('user');
+    counter = 0;
+    reset.classList.remove('show');
 })
